09b9c832f7a6ea2ea2371596cc7216daf3ece70d
Ying Sheng <sqy1415@gmail.com>
2023-11-16 07:19:42 +0000

    update benchmark

--- fair_bench/run_exp.py
+++ fair_bench/run_exp.py
@@ -13,6 +13,7 @@ import numpy as np
 import os
 import sys
 import time
+from dataclasses import dataclass, asdict
 from tqdm import tqdm
 from typing import List, Tuple
 
@@ -25,18 +26,21 @@ from slora.utils.metric import reward, attainment_func
 
 GB = 1024 ** 3
 
-# (prompt len, output len, latency)
-vllm_packed_adapter_dir_to_url_map = {}
 
-def get_peak_mem(server):
-    url = server + "/get_peak_mem"
-    response = requests.post(url)
-    return response.json()["peak_mem"]
+@dataclass
+class Response:
+    adapter_dir: str
+    prompt_len: int
+    output_len: int
+    request_latency: float
+    first_token_latency: float
+    req_time: float
 
 
 async def send_request(
     backend: str,
     server: str,
+    req_time: float,
     req_id: str,
     model_dir: str,
     adapter_dir: str,
@@ -48,12 +52,7 @@ async def send_request(
     request_start_time = time.time()
     headers = {'Content-Type': 'application/json'}
     headers = {"User-Agent": "Benchmark Client"}
-    if backend == "vllm":
-        url = server + "/generate"
-    elif backend == "vllm-packed":
-        url = vllm_packed_adapter_dir_to_url_map[adapter_dir] + "/generate"
-    else:
-        url = server + "/generate_stream"
+    url = server + "/generate_stream"
     
     if backend in ["slora"]:
         data = {
@@ -68,22 +67,8 @@ async def send_request(
             },
             'req_id': req_id,
         }
-    elif backend in ["lightllm"]:
-        data = {
-            'inputs': prompt,
-            'parameters': {
-                'do_sample': False,
-                'ignore_eos': True,
-                'max_new_tokens': output_len,
-                 # 'temperature': 0.1,
-            },
-        }
-    elif backend in ["vllm", "vllm-packed"]:
-        data = {
-            'prompt': prompt,
-            'max_tokens': output_len,
-            'ignore_eos': True,
-        }
+    else:
+        raise NotImplementedError()
 
     first_token_latency = None
     timeout = aiohttp.ClientTimeout(total=3 * 3600)
@@ -110,9 +95,11 @@ async def send_request(
 
     request_end_time = time.time()
     request_latency = request_end_time - request_start_time
-    print(f"req_id {req_id} adapter_dir {adapter_dir} prompt_len {prompt_len} output_len {output_len} "
+    print(f"req_id {req_id} req_time {req_time} adapter_dir {adapter_dir} "
+          f"prompt_len {prompt_len} output_len {output_len} "
           f"request_latency {request_latency:.2f} s, first_token_latency {first_token_latency:.2f} s")
-    return (adapter_dir, prompt_len, output_len, request_latency, first_token_latency)
+    return Response(adapter_dir, prompt_len, output_len,
+                    request_latency, first_token_latency, req_time)
 
 
 async def benchmark(
@@ -128,106 +115,90 @@ async def benchmark(
         if debug:
             print(f"{req.req_id} {req.req_time:.5f} wait {start + req.req_time - time.time():.5f} "
                   f"{req.adapter_dir}")
-        task = asyncio.create_task(send_request(backend, server,
-                                                req.req_id, req.model_dir, req.adapter_dir, req.prompt,
-                                                req.prompt_len, req.output_len, debug))
+        task = asyncio.create_task(send_request(backend, server, req.req_time,
+                                                req.req_id, req.model_dir, req.adapter_dir,
+                                                req.prompt, req.prompt_len, req.output_len,
+                                                debug))
         tasks.append(task)
-    latency = await asyncio.gather(*tasks)
-    return latency
+    responses = await asyncio.gather(*tasks)
+    return responses
 
 
 def get_adapter_dirs(num_adapters, adapter_dirs, backend=None):
     ret = []
     num_iter = num_adapters // len(adapter_dirs) + 1
 
-    if backend == "vllm-packed":
-        num_iter = num_adapters // len(adapter_dirs)
-
     for i in range(num_iter):
         for adapter_dir in adapter_dirs:
             ret.append(adapter_dir + f"-{i}")
     return ret
 
-def get_res_stats(per_req_latency, benchmark_time, backend, warmup_time=0, warmup_num=0):
+def get_res_stats(responses, benchmark_time, backend):
     # get throughput
-    num_abort = len([i for i in per_req_latency if i[4] is None])
-    per_req_latency = [i for i in per_req_latency if i[4] is not None]
-    throughput = len(per_req_latency) / benchmark_time
-    # print(per_req_latency)
-    # if backend == "slora":
-    #     peak_mem = get_peak_mem(server)
-    #     print(f"GPU peak memory (GB):", [[f"{x / GB:.2f}" for x in tpg] for tpg in peak_mem])
+    num_abort = len([x for x in responses if x.first_token_latency is None])
+    responses = [x for x in responses if x.first_token_latency is not None]
+    throughput = len(responses) / benchmark_time
+    # print(responses)
     print(f"Total time: {benchmark_time:.2f} s")
     print(f"Aborted Request: {num_abort}")
     print(f"Throughput: {throughput:.2f} requests/s")
 
-    strip_throughput = (len(per_req_latency) - warmup_num * 2) / (benchmark_time - warmup_time * 2)
-    print(f"Throughput strip: {strip_throughput:.2f} requests/s")
-
     # compute the latency statistics.
-    avg_latency = np.mean([latency for _, _, _, latency, _ in per_req_latency])
+    avg_latency = np.mean([x.request_latency for x in responses])
     print(f"Average latency: {avg_latency:.2f} s")
     avg_per_token_latency = np.mean([
-        latency / (prompt_len + output_len)
-        for _, prompt_len, output_len, latency, _ in per_req_latency
+        x.request_latency / (x.prompt_len + x.output_len)
+        for x in responses
     ])
     print(f"Average latency per token: {avg_per_token_latency:.2f} s")
     avg_per_output_token_latency = np.mean([
-        latency / output_len
-        for _, _, output_len, latency, _ in per_req_latency
+        x.request_latency / x.output_len
+        for x in responses
     ])
     print("Average latency per output token: "
           f"{avg_per_output_token_latency:.2f} s")
 
     # compute the first token latency
-    first_token_latency = [latency for _, _, _, _, latency in per_req_latency]
+    first_token_latency = [x.request_latency for x in responses]
     avg_first_token_latency = np.mean(first_token_latency)
     print(f"Average first token latency: {avg_first_token_latency:.2f} s")
     print(f"90 percentile first token latency: < {np.percentile(first_token_latency, 90):.2f} s")
     print(f"50 percentile first token latency: < {np.percentile(first_token_latency, 50):.2f} s")
 
     # compute the attainment
-    attainment = [attainment_func(latency) for _, _, _, _, latency in per_req_latency]
+    attainment = [attainment_func(x.request_latency) for x in responses]
     avg_attainment = np.mean(attainment)
     print(f"Average attainment: {avg_attainment:.2f}")
 
     # compute the per adapter first token latency (ftl) and attainment
     ftl_per_adapter = {}
     attainment_per_adapter = {}
-    for adapter_dir, _, _, _, latency in per_req_latency:
-        if adapter_dir not in ftl_per_adapter:
-            ftl_per_adapter[adapter_dir] = [latency]
-            attainment_per_adapter[adapter_dir] = [attainment_func(latency)]
+    for x in responses:
+        if x.adapter_dir not in ftl_per_adapter:
+            ftl_per_adapter[x.adapter_dir] = [x.request_latency]
+            attainment_per_adapter[x.adapter_dir] = [attainment_func(x.request_latency)]
         else:
-            ftl_per_adapter[adapter_dir].append(latency)
-            attainment_per_adapter[adapter_dir].append(attainment_func(latency))
+            ftl_per_adapter[x.adapter_dir].append(x.request_latency)
+            attainment_per_adapter[x.adapter_dir].append(attainment_func(x.request_latency))
     for k, v in ftl_per_adapter.items():
         print(f"Average first token latency ({len(v)}) for adapter {k}: {np.mean(v)} s")
     for k, v in attainment_per_adapter.items():
         print(f"Average attainment ({len(v)}) for adapter {k}: {np.mean(v)}")
 
     # dump results
-    if backend == "slora":
-        # TODO
-        # single_gpu_peak_mem = peak_mem
-        single_gpu_peak_mem = 0
-    else:
-        single_gpu_peak_mem = 0
-
-    result = {"total_time": benchmark_time, "gpu_peak_mem": single_gpu_peak_mem, "num_abort": num_abort,
-              "throughput": throughput, "strip_throughput": strip_throughput,
+    result = {"total_time": benchmark_time, "num_abort": num_abort,
+              "throughput": throughput,
               "avg_latency": avg_latency, "avg_per_token_latency": avg_per_token_latency,
               "avg_per_output_token_latency": avg_per_output_token_latency,
               "avg_first_token_latency": avg_first_token_latency,
-              "avg_attainment": avg_attainment}
+              "avg_attainment": avg_attainment,
+              "responses": [asdict(x) for x in responses]}
     res = {"config": to_dict(config), "result": result}
     
     return res
 
 
 def run_exp(model_setting, backend, server, config, output, mode, seed=42, debug=False):
-    if mode == "real":
-        print("*** num_adapters, cv and alpha are not used in real mode ***")
     print([(k, v) for k, v in zip(BenchmarkConfig._fields, config)])
 
     num_adapters, alpha, req_rate, cv, duration, input_range, output_range = config
@@ -248,39 +219,20 @@ def run_exp(model_setting, backend, server, config, output, mode, seed=42, debug
         avg_len = np.mean([req.prompt_len + req.output_len for req in requests])
         print("avg_len:", avg_len, "avg_prompt_len:", avg_prompt_len, "avg_output_len:", avg_output_len)
     else:
-        # first generate your data using real_trace/clean_chat_data.py
-        base_model = BASE_MODEL[model_setting]
-        adapter_dirs = LORA_DIR[model_setting]
-        adapter_dirs, requests = get_real_requests(trace_file="../../../real_trace/clean_chat_conv_20231019.json",
-                                                   req_rate=req_rate, duration=duration,
-                                                   base_model=base_model, adapter_dirs=adapter_dirs,
-                                                   input_range=input_range, output_range=output_range,
-                                                   seed=seed)
-        # print(requests)
-        avg_prompt_len = np.mean([req.prompt_len for req in requests])
-        avg_output_len = np.mean([req.output_len for req in requests])
-        avg_len = np.mean([req.prompt_len + req.output_len for req in requests])
-        print("num_adapters", len(adapter_dirs), "num_requests", len(requests), "avg_len:", avg_len, "avg_prompt_len:", avg_prompt_len, "avg_output_len:", avg_output_len)
-        
+        raise NotImplementedError()
+       
     if debug:
         print("num requests:", len(requests))
         for req in requests[:4]:
             print(req)
 
-    if backend == "vllm-packed":
-        for i in range(len(adapter_dirs)):
-            vllm_packed_adapter_dir_to_url_map[adapter_dirs[i][1]] = f"http://127.0.0.1:{8000 + i}"
-
     # benchmark
     benchmark_start_time = time.time()
-    per_req_latency = asyncio.run(benchmark(backend, server, requests, debug))
+    responses = asyncio.run(benchmark(backend, server, requests, debug))
     benchmark_end_time = time.time()
     benchmark_time = benchmark_end_time - benchmark_start_time
 
-    warmup_time = 10
-    warmup_num = int(req_rate * warmup_time)
-    res = get_res_stats(per_req_latency, benchmark_time, backend,
-                        warmup_time=warmup_time, warmup_num=warmup_num)
+    res = get_res_stats(responses, benchmark_time, backend)
 
     with open(output, "a") as f:
         f.write(json.dumps(res) + "\n")
@@ -288,8 +240,7 @@ def run_exp(model_setting, backend, server, config, output, mode, seed=42, debug
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--backend", type=str, default="slora",
-                        choices=["slora", "vllm", "lightllm", "vllm-packed"])
+    parser.add_argument("--backend", type=str, default="slora")
     parser.add_argument("--suite", type=str, default="default")
 
     parser.add_argument("--model-setting", type=str, default="S1")
